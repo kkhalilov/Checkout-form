d87612d935d95dca8c90dce392f091c131d7cf0c
khalil <khalil.ov@yandex.ru>
2018-04-16 19:26:53 +0300

    qwe

---
+++ app/scripts/main.js
@@ -0,0 +1,22 @@
+$('.btnNext').click(function(){
+    $('.nav-tabs > .active').removeClass('active').next('li').find('a').trigger('click').closest('li').addClass('active');
+    $('.tab-content > .active').removeClass('show active').next('div').addClass('show active');
+});
+$('#firstName, #lastName').on('keyup', function () {
+    var reg = /\d/g;
+    if (this.value.search(reg) !=  -1) {
+        this.value  =  this.value.replace(reg, '');
+    }
+
+});
+$('#postalCode').on('keyup', function () {
+    var reg = /[a-zA-Zа-яА-Я]/g;
+    if (this.value.search(reg) !=  -1) {
+        this.value  =  this.value.replace(reg, '');
+    }
+
+});
+$('#phone').mask('+7(999)999-9999');
+$('#cardNumber').mask('9999 9999 9999 9999');
+$('#cardDate').mask('99 / 99');
+$('#cardCode').mask('999');
